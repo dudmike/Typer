785e2557a7ade7a0efc80ec1402dead455e32358
dudmike <perfect-mira@yandex.ru>
2016-04-12 12:25:18 +0200

    commented end method

--- Scripts/classHero.js
+++ Scripts/classHero.js
@@ -240,7 +240,7 @@ Hero.prototype.back = function() {
 	this.showDistance();
 	this.positionChange(-257);
 }
-//Regenerate all bar's width
+//Update all bar's width
 Hero.prototype.regeneration = function() {
 	if(gameStatus != 1) return;
 
@@ -250,18 +250,21 @@ Hero.prototype.regeneration = function() {
 
 	for(var i=0; i<current_val_array.length; i++) {
 		if(current_val_array[i] >= max_val_array[i]) continue;
-
+		//Select HUD
 		var table;
 		self == me? table = document.querySelectorAll('.HUD')[0]:
 		table = document.querySelectorAll('.HUD')[1];
-
+		//Calculate 5%of max value + property value 
 		current_val_array[i] += max_val_array[i]*5/100;
 		i==0? self.health = current_val_array[i]: i==1? self.energy = current_val_array[i]:
 		self.stamina = current_val_array[i];
-		
+		//Calculate current value percentage of max value
 		var percentage = current_val_array[i]*100/max_val_array[i];
+		//Set up bar's width
 		table.rows[i].querySelector('div[class $="bar"]').style.width = percentage + '%';
+		//Display changes
 		table.querySelectorAll('div[class="num"]')[i].innerHTML = Math.round(current_val_array[i]);
+		//If it's going over the limit
 		if(current_val_array[i] > max_val_array[i]) {
 			table.querySelectorAll('div[class="num"]')[i].innerHTML = max_val_array[i];
 			i==0? self.health = max_val_array[i]: i==1? self.energy = max_val_array[i]:
@@ -270,13 +273,15 @@ Hero.prototype.regeneration = function() {
 	}
 	
 }
-
+//Ends the game
 Hero.prototype.end = function() {
+	//Button to start new game
 	var button = document.createElement('button');
 	
 	button.innerHTML = 'Start new';
 	
 	button.addEventListener('click', function() {
+		//Cancel regeneration
 		clearInterval(regMe);
 		clearInterval(regEnemy);
 		gameStatus = 1;
@@ -285,13 +290,15 @@ Hero.prototype.end = function() {
 		enemy = new Barbarian();
 		log_container.innerHTML = '';
 		var bars = document.querySelectorAll('div[class $="bar"]');
+		//Refresh bars
 		for(var i=0; i<bars.length; i++) {
 			bars[i].style.width= '100%';
 		}
-
+		//Refresh distance
 		var dist = document.getElementById('dist');
 		dist.innerHTML ='<b style="font-size:25px">' + distance + '</b>';
 		var divs = document.querySelectorAll('.num');
+		//Refresh values of basic parameters
 		var arr = [me.maxhealth, me.maxenergy, me.maxstamina, enemy.maxhealth, enemy.maxenergy, enemy.maxstamina];
 		for(var i=0; i<arr.length/2; i++) {
 			divs[i].innerHTML = arr[i];
@@ -300,21 +307,25 @@ Hero.prototype.end = function() {
 		for(var i=3; i<arr.length; i++) {
 			divs[i].innerHTML = arr[i];
 		}
-		 regMe = setInterval(me.regeneration.bind(me), 5000);
-		 regEnemy = setInterval(enemy.regeneration.bind(enemy), 5000);
-
-		 player1.style.left = me.player1_start_position;
-		 player2.style.left = enemy.player2_start_position;
-
-		 me.reloadPlacer();
-		 enemy.react_on_danger();
-		 enemy.bot();
+		//Start new regeneration
+		regMe = setInterval(me.regeneration.bind(me), 5000);
+		regEnemy = setInterval(enemy.regeneration.bind(enemy), 5000);
+		//Start positions
+		player1.style.left = me.player1_start_position;
+		player2.style.left = enemy.player2_start_position;
+		//Set up animation blocks
+		me.reloadPlacer();
+		//Bot mode on
+		enemy.react_on_danger();
+		enemy.bot();
+		//Remove end block
 		document.body.removeChild(document.querySelector('.endBlock'));
 		
 	});
 	
 	var abilities = document.querySelectorAll('.ability');
 	if(enemy.health <= 0) {
+		//End block
 		var div = document.createElement('div');
 		div.style.bottom = '200px';
 		div.style.right =window.innerWidth/2 + 'px';
@@ -324,13 +335,14 @@ Hero.prototype.end = function() {
 		document.body.insertBefore(div, animation_container);
 		this.eventObj.end = 'Passionately swinging with your sword,  you win this battle';
 		this.showEvent(this.eventObj.end);
-		
+		//Stop abilities animation 
 		for(var i=0; i<abilities.length; i++) {
 			document.body.removeChild(abilities[i]);
 		}
 		gameStatus = 0;
 		 
 	} else if(me.health <=0) {
+		//End block
 		var div = document.createElement('div');
 		div.style.bottom = '200px';
 		div.style.right =window.innerWidth/2 + 'px';
@@ -340,6 +352,7 @@ Hero.prototype.end = function() {
 		document.body.insertBefore(div, animation_container);
 		this.eventObj.end = 'You were good, but destiny is cruel...';
 		this.showEvent(this.eventObj.end);
+		//Stop abilities animation
 		for(var i=0; i<abilities.length; i++) {
 			document.body.removeChild(abilities[i]);
 		}
